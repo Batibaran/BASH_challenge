b2ac1163a1dd49e44fa8472ffc84aeaab9855e86
Baran Bati <66915560+Batibaran@users.noreply.github.com>
2023-10-27 12:48:36 +0300

    Update install-docker.sh
    add docker compose

--- install-docker.sh
+++ install-docker.sh
@@ -23,5 +23,11 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Print status
 docker --version
+docker-compose --version
 sudo systemctl status docker
